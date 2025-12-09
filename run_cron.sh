cdae6b6fc05aacf6cc28265a81a01adf97ed626d
999k923 <419879397@qq.com>
2025-12-09 21:52:45 +0800

    Update run_cron.sh

--- run_cron.sh
+++ run_cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    echo "[Cron] 执行 reset_id_only_safe.py ..."
+    echo "[Cron] 执行 reset_node_id.py ..."
     python3 /app/instance/reset_node_id.py
     echo "[Cron] 完成，等待 1 小时..."
     sleep 3600
